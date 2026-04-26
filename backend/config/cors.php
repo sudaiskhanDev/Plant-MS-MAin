d42df3dad16a96fca24ea3999e7b614032819a2b
sudaiskhanDev <sudaisliveofficial@gmail.com>
2026-04-26 22:05:40 +0500

    Updated code with new features/fixes

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -7,9 +7,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        'http://127.0.0.1:5500'
-    ],
+     'allowed_origins' => ['*'], // sab origins allow
 
     'allowed_headers' => ['*'],
 
