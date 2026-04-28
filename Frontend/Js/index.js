eb4e497c0f1f499e64619d64a74c116f04b12d91
sudaiskhanDev <sudaisliveofficial@gmail.com>
2026-04-28 10:57:39 +0500

    final update

--- Frontend/Js/index.js
+++ Frontend/Js/index.js
@@ -55,3 +55,50 @@ async function logout() {
     }
 }
  
+
+
+
+ 
+const API = "http://127.0.0.1:8000/api/plants";
+
+loadLatestProducts();
+
+async function loadLatestProducts() {
+
+    const res = await fetch(API);
+    const data = await res.json();
+
+    const container = document.getElementById("latestProducts");
+    const template = document.getElementById("productTemplate");
+
+    container.innerHTML = ""; // clear
+
+    // latest 4 products
+    const latest = data.slice(-4).reverse();
+
+    latest.forEach(p => {
+
+        let clone = template.cloneNode(true);
+        clone.style.display = "block";
+        clone.removeAttribute("id");
+
+        // DATA SET
+        clone.querySelector(".product-name").textContent = p.name;
+        clone.querySelector(".product-price").textContent = "$" + p.price;
+
+        let img = p.image 
+            ? "http://127.0.0.1:8000/storage/" + p.image 
+            : "https://via.placeholder.com/200";
+
+        clone.querySelector(".product-img").src = img;
+
+        // 🔥 CLICK → DETAILS PAGE
+        clone.style.cursor = "pointer";
+        clone.onclick = () => {
+            window.location.href = "./Pages/Product details.html?id=" + p.plant_id;
+        };
+
+        container.appendChild(clone);
+    });
+}
+ 
